3f38cef0b0c938d8c25a9fb0d862c67aaa83fca4
NIrmal Raja <nirmalraja9111989@gmail.com>
2026-01-28 10:09:24 +0530

    Update miniproject.py

--- miniproject.py
+++ miniproject.py
@@ -3,5 +3,7 @@ def welcome():
     print("   Welcome to My Python Project   ")
     print("===================================")
 print("i am sharukesan")
+print("Receive file sharu")
 welcome()
+
 
