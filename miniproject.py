c9efc8f69ebfdcecad4a7a750b9b6c0a553ddb19
sharukesanff-cmyk <sharukesanff@gmail.com>
2026-01-28 10:08:00 +0530

    Update miniproject.py

--- miniproject.py
+++ miniproject.py
@@ -2,5 +2,6 @@ def welcome():
     print("===================================")
     print("   Welcome to My Python Project   ")
     print("===================================")
-
+print("i am sharukesan")
 welcome()
+
